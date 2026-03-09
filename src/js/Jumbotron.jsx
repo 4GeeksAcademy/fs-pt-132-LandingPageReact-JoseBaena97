a5f9416d13f1374f66da2141c06c422e483feb0d
JoseBaena97 <josembaena97@gmail.com>
2026-03-09 20:15:30 +0000

    feat: added navbar & jumbo

---
+++ src/js/Jumbotron.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Jumbo = () => {
+
+    return (
+        <div className="container pt-3 col-12">
+
+            <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+                <div className="container-fluid py-5">
+                    <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+                    <p>
+                        Using a series of utilities, you can create this jumbotron,
+                        just like the one in previous versions of Bootstrap.
+                        Check out the examples below for how you can remix and restyle it to your liking.
+                    </p>
+                    <button className="btn btn-primary btn-lg" type="button">Example button</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbo;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,12 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import Navbar from './Nabvar';
+import Jumbo from './Jumbotron';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Navbar/>
+    <Jumbo/>
   </React.StrictMode>,
 )
